d3742aaf2d46f5ba30744ee3d13a62bb8ed4534c
Lacrita24 <jhoy16ps4@gmail.com>
2023-09-07 14:10:44 -0400

    Implementando el apartado de servicios, en la carpeta de back

---
+++ omni-back/Servicio.js
@@ -0,0 +1,57 @@
+// models/Servicio.js
+const mongoose = require('mongoose');
+
+const servicioSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  descripcion_tecnica: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  tipo_servicio: {
+    type: String,
+    required: true,
+  },
+  tipo_plataforma: {
+    type: String,
+    required: true,
+  },
+  tarifa: {
+    type: String,
+    required: true,
+  },
+  pre_requisito: {
+    type: String,
+    required: true,
+  },
+  disponibilidad: {
+    type: String,
+    required: true,
+  },
+  costos: {
+    type: Number,
+    required: true,
+  },
+  prioridad: {
+    type: String,
+    required: true,
+  },
+  tiempo_estimado: {
+    type: String,
+    required: true,
+  },
+});
+
+const Servicio = mongoose.model('Servicio', servicioSchema);
+
+module.exports = Servicio;
