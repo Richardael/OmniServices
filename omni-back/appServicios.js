b422f59a6caa8c99c150e6205613ed4dbba1f35d
Heraldsito <richardael14@gmail.com>
2023-09-11 12:12:16 -0400

    Filtro de Open e ibm

--- omni-back/appServicios.js
+++ omni-back/appServicios.js
@@ -5,7 +5,7 @@ const RegistroServiciosModel = require('./Modelo/RegistroServicios'); // Importa
 const ServiciosModel = require('./Modelo/Servicios'); // Importa el modelo de servicios
 const cors = require('cors'); // Importa el módulo 'cors'
 // Conexión a la base de datos Omniservices
-const uri = "mongodb+srv://desabytes:Melon24@omnicluster.hl7ynvt.mongodb.net/OmniServices?retryWrites=true&w=majority";
+const uri = "mongodb+srv://jhoysantaella15:Melon24@cluster0.zrpgq2r.mongodb.net/Omniservices?retryWrites=true&w=majority";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -28,10 +28,22 @@ app.get('/servicios', async (req, res) => {
     // Obtén todos los servicios desde la base de datos
     const servicios = await ServiciosModel.find();
     res.json(servicios);
-    console.log("Bien Hecho");
+    console.log("Pasaste por busqueda IBM");
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Hubo un error al obtener los servicios' });
+  }
+});
+
+app.get('/servicios/open', async (req, res) => {
+  try {
+    // Obtén todos los servicios desde la base de datos
+    const serviciosOpen = await ServiciosModel.find({ tipo_plataforma: "Open" });
+    res.json(serviciosOpen);
+    console.log("Busqueda Open");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Hubo un error al obtener los servicios de open' });
   }
 });
 
@@ -49,6 +61,7 @@ app.post('/registro', async (req, res) => {
 
     // Respuesta exitosa
     res.status(201).json({ message: 'Usuario registrado con éxito' });
+    console.log("Registro exitoso")
   } catch (error) {
     console.error(error);
     // Manejo de errores
@@ -59,5 +72,5 @@ app.post('/registro', async (req, res) => {
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor en ejecución en http://192.168.1.50:${PORT}`);
+  console.log(`Servidor en ejecución en http://192.168.1.43:${PORT}`);
 });
