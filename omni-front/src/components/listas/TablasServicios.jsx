d6de35bd513374883bceb6ade122e5cfe4ee871f
Heraldsito <richardael14@gmail.com>
2023-09-18 11:07:08 -0400

    Tabla Dinamica

--- omni-front/src/components/listas/TablasServicios.jsx
+++ omni-front/src/components/listas/TablasServicios.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { RiArrowDropUpLine, RiArrowDropDownLine } from 'react-icons/ri';
 
 const TablasServicios = () => {
   return (
-    <div className='"relative overflow-x-auto shadow-md sm:rounded-lg"'>
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className='w-full text-sm text-left text-secondary-500'>
             <thead className='text-xs text-secondary-900 uppercase bg-gray-50 "'>
             <tr>
@@ -12,25 +13,19 @@ const TablasServicios = () => {
                 <th scope="col" className="px-6 py-3">
                     <div className="flex items-center">
                         Plataforma del Servicio
-                        <a href="#"><svg className="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-  </svg></a>
+                        <RiArrowDropUpLine className="w-7 h-7 ml-1.5" />
                     </div>
                 </th>
                 <th scope="col" className="px-6 py-3">
                     <div className="flex items-center">
                         Categoria Servicio
-                        <a href="#"><svg className="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-  </svg></a>
+                        <RiArrowDropDownLine className="w-7 h-7 ml-1.5" />
                     </div>
                 </th>
                 <th scope="col" className="px-6 py-3">
                     <div className="flex items-center">
                         Prioridad
-                        <a href="#"><svg className="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-  </svg></a>
+                        <RiArrowDropDownLine className="w-7 h-7 ml-1.5" />
                     </div>
                 </th>
                 <th scope="col" className="px-6 py-3">
@@ -38,6 +33,25 @@ const TablasServicios = () => {
                 </th>
             </tr>
         </thead>
+        <tbody>
+            <tr className="bg-white border-b border-secondary-300">
+                <th scope="row" className="px-6 py-4 font-medium text-primary-300 whitespace-nowrap">
+                    Apple MacBook Pro 17"
+                </th>
+                <td className="px-6 py-4">
+                    Silver
+                </td>
+                <td className="px-6 py-4">
+                    Laptop
+                </td>
+                <td className="px-6 py-4">
+                    $2999
+                </td>
+                <td className="px-6 py-4 text-right">
+                    <h1 href="#" className="font-medium text-primary-400  hover:underline cursor-pointer">Edit</h1>
+                </td>
+            </tr>
+        </tbody>
         </table>
     </div>
   )
--- omni-front/src/pages/admin/ListaServicios.jsx
+++ omni-front/src/pages/admin/ListaServicios.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import TablasServicios from '../../components/listas/TablasServicios'
+import BuscadorLista from '../../components/listas/BuscadorLista'
 
 
 
 const ListaServicios = () => {
   return (
     <div>
-      <h1 className="text-3xl font-extrabold text-center text-primary-900 mb-5">
+    <div className='flex'>
+      <BuscadorLista />
+      <h1 className="text-3xl font-extrabold mx-auto text-primary-900 mb-5">
         Lista <span className="text-primary-300"> Servicios</span>
       </h1>
+      </div>
       <TablasServicios />
     </div>
   )
