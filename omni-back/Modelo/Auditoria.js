57a16df2697888cb14102eeb37f58be11ea4a28c
Lacrita24 <jhoy16ps4@gmail.com>
2023-09-22 16:40:24 -0400

    Modificando el modelo de auditoria.js para la auditoria de modificar

--- omni-back/Modelo/Auditoria.js
+++ omni-back/Modelo/Auditoria.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 
 const auditoriaSchema = new mongoose.Schema({
-    usuario: {
-      type: String,
-      ref: 'usuarios', // Suponiendo que tienes un modelo de Usuarios
-    },
-    accion: String, // Aquí puedes registrar la acción realizada
-    tipoDocumento: String,
-    documentoAfectado: {
-      type: mongoose.Schema.Types.ObjectId,
-      refPath: 'tipoDocumento', // Referencia dinámica a la colección (Servicios o Talleres)
-    },
-    nombreDocumento: String,
-    fechaHora: { type: Date, default: Date.now },
-    detalles: String,
-  });
+  usuario: {
+    type: String,
+    ref: 'usuarios', // Suponiendo que tienes un modelo de Usuarios
+  },
+  accion: String, // Aquí puedes registrar la acción realizada
+  tipoDocumento: String,
+  documentoAfectado: {
+    type: mongoose.Schema.Types.ObjectId,
+    refPath: 'tipoDocumento', // Referencia dinámica a la colección (Servicios o Talleres)
+  },
+  nombreDocumento: String,
+  fechaHora: { type: Date, default: Date.now },
+  detalles: String,
+  datosCambiados: {
+    type: Object, // Objeto JavaScript para almacenar los cambios
+    default: null, // Puedes establecer un valor predeterminado como nulo si no se proporciona
+  },
+});
 
   const AuditoriaModel = mongoose.model('auditoria', auditoriaSchema);
 
